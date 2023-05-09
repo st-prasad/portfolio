98a68a0b0b5967f411fb1a5635d53f5d89c1ea0a
st-prasad <trindo.me@gmail.com>
2023-05-10 00:56:37 +0530

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <div className="App">
       <Navbar />
       {/* <Nav /> */}
-      {/* <HeroPage /> */}
+      <HeroPage />
       {/* <Projects/> */}
       {/* <SkillBars/> */}
       {/* <About /> */}
