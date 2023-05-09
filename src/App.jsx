0cee218acdf7eae9f5f67d147880c6b78ede48da
st-prasad <trindo.me@gmail.com>
2023-05-09 23:57:38 +0530

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@
 // import Contact from './components/Contact'
 // import Contact from 'src/components/Contact'
 // import HeroPage from 'src/components/HeroPage'
-import Navbar from './portfolioComponents/Navbar'
+import Navbar from './components/CategoryButton'
 // import Projects from './Components/Projects'
 // import Skills from 'src/components/Skills'
 // import Card from './projectComponents/Card'
